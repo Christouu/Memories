dcf4f8b24a178ed8c85d2772e2edfab0fc21661b
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-04-29 12:32:26 +0300

    added pagination and search input and  search tags with chip-input

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 function App() {
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/auth" element={<Auth />}></Route>
+          <Route path="/" element={<Navigate to={"/posts"} replace />}></Route>
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/auth" element={!user ? <Auth /> : <Home />}></Route>
         </Routes>
       </Container>
     </BrowserRouter>
